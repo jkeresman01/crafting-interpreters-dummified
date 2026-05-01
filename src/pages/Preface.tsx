c11ba8fa7111a2099b42a4acc7d184cc7bd68169
josip <josipkeresman@gmail.com>
2026-05-01 21:44:02 +0200

    Fix some random nonsene

--- src/pages/Preface.tsx
+++ src/pages/Preface.tsx
@@ -71,7 +71,8 @@ export default function Preface() {
               title="YouTube Short"
               frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-              allowfullscreen>
+              allowFullScreen
+              style={{ borderRadius: '8px', border: '1px solid var(--border)' }}>
           </iframe>
         </Callout>
 
