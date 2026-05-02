4a33ee31a500a5021f06967e0a19a8b4f2ff398d
josip <josipkeresman@gmail.com>
2026-05-02 20:57:15 +0200

    Improve some wording in preface

--- src/pages/Preface.tsx
+++ src/pages/Preface.tsx
@@ -5,24 +5,30 @@ export default function Preface() {
     <>
       <h1>How did it start?</h1>
 
-      <p>
-          Yesterday I closed one important chapter of my life. I finished my first
-          book and left with one last piece of wisdom that will carry me through
-          this one: <i>"<strong>Just talk, don't walk</strong>: The continuation of
-          the thought, 5 languages one interpreter"</i>
+      <p> Yesterday I closed one important chapter of my life. I finished my first
+        book and left with one last piece of wisdom that will carry me through this one:
+        <i>"<strong>Just talk, don't walk</strong>: The continuation of the thought,
+        5 languages one interpreter"</i>
       </p>
 
       <p>
         That book started with a funny story; this one is a bit different, so let's
         go back to where it started. That same <i>"closing chapter day"</i>. I was
-        sipping coffee, talking nonsense in the same bar — perhaps more of a <i>"hole"</i> —
-        and decided to go for a walk, you know, to shift back into the world of
-        thought. Just before my return to the said "hole", I was struck by lightning:
-        that one smoking hot red-haired badass goddess I just saw. The weather was fairly 
-        sunny that day and I continued to walk but that single moment brought back my inspiration to 
-        write and the idea to dummify the concepts taught in the "Crafting Interpreters" book, 
-        speak a bit about different programming languages, how to implement an interpreter in them,
-        and to answer the question: "Why do I walk? Is it to bring me into the world of thought?"
+        sipping coffee, talking nonsense in some bar — perhaps more of a <i>"hole"</i>
+        — and decided to go for a walk, you know, to shift back into the world of
+        thought. Just before my return to the said <i>"hole"</i>, I was struck by
+        lightning: that one smoking hot red-haired badass goddess I just saw. Ofcourse
+        struck by a lightning in world of tought, in real world I was just on crosswalk
+        and day was fairly sunny so I just continued to walk, but ohhh boy even in the 
+        world of tought my mind was blown, you know the vibe and all.
+      </p>
+
+      <p>
+        That  moment brought back my inspiration to write and the idea to dummify
+        the concepts taught in the <i>"Crafting Interpreters"</i> book, speak a bit about
+        different programming languages, how to implement an interpreter in them
+        and to answer the question: "Why do I walk? Is it to bring me into the world
+        of thought?"
       </p>
 
       <Callout type="tip">
@@ -46,19 +52,20 @@ export default function Preface() {
 
       <p>
         Five languages or not — does it really matter today? Given an implementation
-        in one, AI can handle the rest pretty easily.
+        in one, AI can handle the rest pretty easily. But with all of that, the joy
+        of programming has disappeared. <strong>Don't you agree?</strong>  We've lost
+        that sense of building something slowly, step by step. You know To write a
+        small piece of software and you can't really tell if it works — you have to
+        test how it behaves, step by step, day by day.
       </p>
 
       <p>
-        But with all of that, the joy of programming has disappeared. Don't you agree?
-        We've lost that sense of building something slowly, step by step. You write a
-        small piece of software and you can't really tell if it works — you have to
-        test how it behaves. So I'll do it the old way and share some thoughts along
-        the way. Maybe even spin up a debugger — you know, those confusing tools
-        meant for old people and ohhhh boy are they confusing, but the thing about debugging 
-        sessions is to just embrace the meaningless of it all, you don't fight it, you just
-        embrace it and click step over, step into buttons all day along, or in other words
-        you just play along.
+        So I'll do it the old way and share some thoughts along the way.  Maybe even
+        spin up a debugger — you know, those confusing tools meant for old people
+        and ohhhhh boy are they confusing, but the thing about debugging sessions is
+        to just embrace the meaningless of it all, you don't fight it, you just embrace
+        it and click step over, step into buttons all day long, until you realize that
+        you forgot someting stupid seven hours ago.
       </p>
 
       <Callout type="tip">
