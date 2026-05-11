27436cf72c5692f3890c6a770d505191e7c115e0
josip <josipkeresman@gmail.com>
2026-05-12 00:24:47 +0200

    Fix some wording

--- src/pages/SmallBoober.tsx
+++ src/pages/SmallBoober.tsx
@@ -6,8 +6,8 @@ export default function SmallBoober() {
       <p>
         Okay, okay but we need a language in order for this to make any sense, this
         requires huge inspiration from my part, but some things are just boring and
-        dull, like basic data types: booleans, numbers, strings and nils so we can
-        start with that.
+        dull, like basic data types: booleans, numbers, strings and nils|nulls|NULL
+        or however you wan't to call them so we can start with that.
       </p>
     </>
   );
