9c37a08fea13dcfbccd3ee032ad498fb8336865b
josip <josipkeresman@gmail.com>
2026-05-13 12:20:31 +0200

    Improve some additional wording

--- src/pages/smallboober/Introduction.tsx
+++ src/pages/smallboober/Introduction.tsx
@@ -5,10 +5,16 @@ export default function Introduction() {
 
       <p>
         Okay, okay we need an actual language for this to make any sense, we'll use
-        one simple small and cute pretty similar to what "Lox" does, but let's first
-        talk about this new concept of mine that I've been thinking about and then we
-        will move to language design.
-      </p>
+        one simple small and cute pretty similar to what "Lox" does, but "Lox" also
+        pretty much tries to achive what Lua does with it's cool only 21 keywords in
+        the grammar languge design.
+       </p>
+
+        <p>
+         Anyhowzzy let's first talk about this new concept of mine that I've been
+         thinking about, bring in some Adlers tought and talk about language design.
+        </p>
+
     </>
   );
 }
