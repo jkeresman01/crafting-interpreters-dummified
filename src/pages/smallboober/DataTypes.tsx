e1a83427dda83c9aa0315b0c0f6e202f1f75321f
josip <josipkeresman@gmail.com>
2026-05-13 11:53:02 +0200

    Add some boring data types stuff

--- src/pages/smallboober/DataTypes.tsx
+++ src/pages/smallboober/DataTypes.tsx
@@ -1,7 +1,87 @@
+import CodeBlock from '../../components/CodeBlock';
+
 export default function DataTypes() {
   return (
     <>
       <h1>DataTypes</h1>
+
+      <p>Okay, okay there is nothing too smart about this data types,
+         some things are just as they are, we will just follow what "Lox" does in this
+         regard, since that's just how it gets done, can't even bring philisophy in
+         because it's just as boring as that.</p>
+
+      <h2>Booleans</h2>
+
+      <p>One billion years ago, some languages repurposed existing data types to
+      represent what's true and whats false, today most modern languages have dedicated
+      data types, so we will just follow that and use plain simple `True` and `False`</p>
+
+      <CodeBlock
+        language="plaintext"
+        code={`True  will represent truth`}
+      />
+
+      <CodeBlock
+        language="plaintext"
+        code={`False will represent falsehood`}
+      />
+
+      <h2>Numbers</h2>
+       <p>
+         We keep it simple and also follow what "Lox" does, only have one kind of
+         number: double precision floint point.
+       </p>
+
+      <CodeBlock
+        language="plaintext"
+        code={`1234 will be represent integer`}
+      />
+
+      <CodeBlock
+        language="plaintext"
+        code={`12.34 will represent decimal`}
+      />
+
+      <h2>Strings</h2>
+
+      <p>
+        This is even boring and dull, since here most of the langauges use "" for
+        string literals, so we will also follow that.
+      </p>
+
+      <CodeBlock
+        language="plaintext"
+        code={`"She was smoking hot"`}
+      />
+
+      <CodeBlock
+        language="plaintext"
+        code={`"" this is an empty string"`}
+      />
+
+      <CodeBlock
+        language="plaintext"
+        code={`"123" and this is string, not a number`}
+      />
+
+      <h2>Nil</h2>
+
+      <p>We also need built in data type that represent "nothing" or "no value" and most
+      commonly used solutions here are to use Nil or Null and we will just follow what
+      "Lox" as we did so far</p>
+
+      <CodeBlock
+        language="plaintext"
+        code={`Nil is our built in data type of choice for that`}
+      />
+
+     <p>
+      Okay, okay we are done with basic data types, as I said they are just boring and
+      dull but we can't do anything useful without them, but let's move to some more
+      interesting parts.
+     </p>
+
     </>
+
   );
 }
