cb32c19acdbe58fed11f5bc2eadce886d15150ef
josip <josipkeresman@gmail.com>
2026-05-14 21:36:39 +0200

    Add additional toughts

--- src/pages/smallboober/Introduction.tsx
+++ src/pages/smallboober/Introduction.tsx
@@ -4,18 +4,26 @@ export default function Introduction() {
       <h1>Introduction</h1>
 
       <p>
-        Okay, okay we need an actual language for this to make any sense, we'll use
-        one simple small and cute pretty similar to what "Lox" does, but "Lox" also
-        pretty much tries to achieve what Lua does with its cool only 21 keywords in
-        the grammar language design.
+        Okay, okay, we need an actual language for this to make any sense, we'll use
+        one that's simple, small and cute, pretty similar to what "Lox" does, but "Lox"
+        also pretty much tries to achieve what Lua does with its cool only-21-keywords
+        grammar language design.
        </p>
 
         <p>
-         Anyhowzzy, interesting tought about stories before we move to that, It's
-         funny how some stories never were, but tend to stick, stories of regret, same
-         thing for those moments that are whiteboard example of what's kinda cringe,
-         dressed up real nice or not, still adorabale and cute, most memories just fade,
-         while rare ones stick, years have passed and they still stick.
+         Anyhowzzy, interesting thought about stories before we move to that, it's funny
+         how some stories never were, but tend to stick, stories of regret, same thing
+         for those moments that are a whiteboard example of what's kinda cringe, dressed
+         up real nice or not, still adorable and cute, most memories just fade, while
+         rare ones stick, years have passed and they still stick. You see, to come back
+         to the first page and preface, "Why write this part?" this was seen through my
+         eyes, so cute, adorable, gorgeous and unique, but stayed in the world of thought.
+        </p>
+
+        <p>
+         Anyhowzzzy, point of the story, years have passed, she will never know, that
+         she'll read this I highly doubt, back to the main point, when we don't act is
+         what we regret while the rest of the memories just fade away.
         </p>
 
     </>
