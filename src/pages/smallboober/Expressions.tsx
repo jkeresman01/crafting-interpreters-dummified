5780f37225dd020230b0eb5fd8d8b9966a6df994
josip <josipkeresman@gmail.com>
2026-05-18 14:08:39 +0200

    Add additional toughts

--- src/pages/smallboober/Expressions.tsx
+++ src/pages/smallboober/Expressions.tsx
@@ -6,15 +6,17 @@ export default function Expressions() {
       <h1>Expressions</h1>
 
       <p>
-        Let's say hypothetically that this smoking hot red haired goddess
-        that I saw was already in love, I could never take her from another is what
-        Prince also said in his Purple Rain song and he would be right, because that
-        would be wrong and that would cause an effect which is statement's job — not
-        ours, we're here for expressions, and an expression's whole deal is producing
-        a value. In its simplest form <strong>a + b</strong> is an expression, it's got
-        two operands a and b (which makes it a binary one) and its job is to produce a
-        value, the result of a + b, '+' is an operator and the subexpressions on both
-        sides are called operands.</p>
+        Let's say hypothetically that this smoking hot red haired goddess that I saw
+        was already in love, I could never take her from another is what Prince also
+        said in his Purple Rain song and he would be right, because that would be
+        wrong and that would cause an effect which is statement's job — not ours,
+        we're here for expressions, and an expression's whole deal is producing a
+        value. In its simplest form <strong>a + b</strong> is an expression, it's got
+        two operands a and b (which makes it a binary one) and its job is to produce
+        a value, the result of a + b, '+' is an operator and the subexpressions on
+        both sides are called operands. Okay, okay but lets say hypothetically that he
+        wasn't in love than we could express some feeling, you see how I used express,
+        because we are talking about expressions</p>
 
         <h2>Logical operators</h2>
 
