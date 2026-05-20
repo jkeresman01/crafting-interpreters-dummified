5bdc0cca7b591622247149be88785275aedba0ed
josip <josipkeresman@gmail.com>
2026-05-20 12:06:03 +0200

    Fix some random typos

--- src/pages/smallboober/Statements.tsx
+++ src/pages/smallboober/Statements.tsx
@@ -3,10 +3,11 @@ export default function Statements() {
     <>
       <h1>Statements</h1>
 
-      <p>Steve Jobs said that people how are crazy enough to think that
-      they can change the world are the ones who do, that was a statement and statements
-      job is to chnage the world in some sense, usually by modifying 
-      some state, reading input or producing output.</p>
+      <p>Steve Jobs said that people how are crazy enough to think that they can
+      change the world are the ones who do, that was a statement and statements job
+      is to change the world in some sense, usually by modifying some state, reading
+      input or producing output.</p>
+
     </>
   );
 }
