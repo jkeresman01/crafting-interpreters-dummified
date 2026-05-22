2b2285ae290603fcb5b5d3353af0210872066b63
Josip Keresman <josipkeresman@gmail.com>
2026-05-22 11:12:49 +0200

    Add some toughts about control flow
    Added a paragraph about personal experience before discussing control flow.

--- src/pages/smallboober/ControlFlow.tsx
+++ src/pages/smallboober/ControlFlow.tsx
@@ -2,6 +2,13 @@ export default function ControlFlow() {
   return (
     <>
       <h1>Control Flow</h1>
+
+      <p>Okay, okay time for the control flow, but a small story before that,
+      this Wednesday I went for a walk, you know to bring me into the world of tought, 
+      it wasn't that easy and I walk the same route pretty much every now and then,
+      perhaps I eat to much pizza lately and have run out of shape or I don't know, 
+      should've skipped coffee in that bar, anyhoowzzy time for the control flow and you 
+      will see how it all comes together after some viby examples</p>
     </>
   );
 }
