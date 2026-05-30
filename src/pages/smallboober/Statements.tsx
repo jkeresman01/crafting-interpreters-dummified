bbae28dd73309409dfa7b0f2ebb63cade6998f19
josip <josipkeresman@gmail.com>
2026-05-30 07:25:27 +0200

    Add some examples for statements

--- src/pages/smallboober/Statements.tsx
+++ src/pages/smallboober/Statements.tsx
@@ -1,12 +1,69 @@
+import CodeBlock from '../../components/CodeBlock';
+
 export default function Statements() {
   return (
     <>
       <h1>Statements</h1>
 
-      <p>Steve Jobs said that people who are crazy enough to think that they can
-      change the world are the ones who do, that was a statement and a statement's job
-      is to change the world in some sense, usually by modifying some state, reading
-      input or producing output.</p>
+      <p>Steve Jobs said that people who are crazy enough to think that they can change
+      the world are the ones who do, that was a statement and a statement's job is to
+      change the world in some sense, usually by modifying some state, reading input
+      or producing output.</p>
+
+      <p>Okay, okay it will all make sense after some examples as I've said statements
+      cause effects, they don't evaluate to a value, you've already seen at least one
+      through this book, the <strong>print</strong> statement: </p>
+
+      <CodeBlock
+        language="plaintext"
+        code={`print "She was smoking hot" + ", " + " mind blown!;"`}
+      />
+
+      <p>A <strong>print</strong> statement evaluates a single expression and displays
+      the result to the user.</p>
+
+      <h2>Variables</h2>
+
+      <p>As I've said our programming language will be small and cute, it will also
+      be dynamically typed, explained in simple terms, our variables can store values
+      of any type and a single variable can even store values of different types at
+      different times.</p>
+
+      <p>We will follow what "Lox" does and our variables will be declared using
+      <strong>var</strong> statements. Some commonly used alternatives to using var
+      would be let (JavaScript, TypeScript), local (Lua) or to simply use no declaration
+      keyword (Python, Ruby).</p>
+
+      <CodeBlock
+        language="plaintext"
+        code={`var variable = "She was smoking hot";`}
+      />
+
+      <p>If you omit the initializer the variable's value defaults to
+      <strong>nil</strong>.</p>
+
+      <CodeBlock
+        language="plaintext"
+        code={`var nilValue`}
+      />
+
+      <CodeBlock
+        language="plaintext"
+        code={`print "She was smoking hot" + ", " + " mind blown!;"`}
+      />
+
+      <p>Once declared you can naturally access and assign a variable using its name.</p>
+
+
+      <CodeBlock
+        language="plaintext"
+        code={`var variable = "She was smoking hot";`}
+      />
+
+      <CodeBlock
+        language="plaintext"
+        code={`print variable`}
+      />
 
     </>
   );
