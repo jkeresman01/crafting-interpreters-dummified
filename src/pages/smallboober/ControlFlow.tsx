3054ef54ce7613a288f7a036a248c41e6d2b3cf2
josip <josipkeresman@gmail.com>
2026-06-11 23:41:29 +0200

    Rephrase some random nonsene

--- src/pages/smallboober/ControlFlow.tsx
+++ src/pages/smallboober/ControlFlow.tsx
@@ -3,12 +3,10 @@ export default function ControlFlow() {
     <>
       <h1>Control Flow</h1>
 
-      <p>Time for the control flow, but a small story before that,
-      this Wednesday I went for a walk, you know to bring me into the world of thought,
-      it wasn't that easy and I walk the same route pretty much every now and then,
-      perhaps I eat too much pizza lately and am out of shape or I don't know,
-      should've skipped coffee in that bar, anyhowzzy time for the control flow and you
-      will see how it all comes together after some viby examples</p>
+      <p>Yesterday, it was rainy evening in my part of the world, that's for sure,
+      was driving home with interesting tought on my mind, waiting for inspiration to
+      come back.  </p>
+
     </>
   );
 }
